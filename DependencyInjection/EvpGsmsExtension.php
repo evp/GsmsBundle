3788d613a3fad4ec7b4d7ba580b135ecbfaff626
Vytautas Gimbutas <vytautas@gimbutas.net>
2012-07-13 11:50:59 +0300

    initial

---
+++ DependencyInjection/EvpGsmsExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Evp\Bundle\GsmsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class EvpGsmsExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        if (isset($config['credentials']['username'])) {
+            $container->setParameter('evp_gsms.credentials.username', $config['credentials']['username']);
+        }
+
+        if (isset($config['credentials']['password'])) {
+            $container->setParameter('evp_gsms.credentials.password', $config['credentials']['password']);
+        }
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
---
+++ EvpGsmsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Evp\Bundle\GsmsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EvpGsmsBundle extends Bundle
+{
+}
